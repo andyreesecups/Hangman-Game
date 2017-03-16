e3a0826dc0d1d8002478dd7d6f7c908799450893
andyreesecups <andyreesecups@yahoo.com>
2017-03-15 23:34:03 -0500

    updated game.js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,27 @@
 //// create random words
 var options = ["jaime", "tyrion", "cersei", "khaleesi", "ned", "arya", "jon snow", "winterfell", "sansa", "joffrey", "ramsay", "hodor", "bran", "stark"];
+
 //pick random word
 var randomWord = options[Math.floor(Math.random() * options.length)];
+
 // show random word in browser
 console.log("The word is: " + computerGuess);
 
+//every time word is guess wins increases by 1
 var wins = 0;
-var guessesRemaining = 30;
-var guessedLetters = [];
 
+// score keeper
+var guessesRemaining = 30;
+
+//display to user blank fields that match the number of letters.
+var guessedLetters = [];
+guessedLetters.push(userGuess);
 
 	//capture user inputs with keyup.
 	document.onkeyup = function() {
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
     console.log(userGuess);
+
    
    guessedLetters.push(userGuess);
    for(var i = 0; i < options.length; i++){
@@ -24,25 +32,26 @@ var guessedLetters = [];
    	alert("Game over");
    }
 
-   //create a score keeper that decrements with incorrect guesses.
 
 //create a counter that increments with each keyed letter.
 
 
 
-//display to user blank fields that match the number of letters.
 
-//capture user inputs with keyup.
 
-//if user keys a letter contained in the word, display the letter and
-//increment score.
+//if user keys a letter contained in the word, display the letter and increment score.
 
-//if user keys a letter not in word, decrement score keeper.
+//if user keys a letter not in word, decrement guessesRemaining.
 
-//if score keeper reaches 0, end game and alert "you lose".
-
+//if guessesRemaining reaches 0, end game and alert "you lose".
+if(guessesRemaining === 0){
+  alert("gameOver");
+}
 //if all letters are selected for the word, alert "you win" and start game over. 
-
+if(userGuess === randomWord.length){
+  alert("Great job!");
+  win++;
+}
 
     
 
